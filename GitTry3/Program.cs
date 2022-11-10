a0c49e2a74cd1737af60e22490ac3c37e86911d7
VaibhaV3191 <vaibhav.shinde@vhaashtech.com>
2022-11-10 13:26:46 +0530

    Vaibhav First Commit

--- GitTry3/Program.cs
+++ GitTry3/Program.cs
@@ -7,6 +7,8 @@ namespace GitTry3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Master");
+            Console.WriteLine("Hello Vaibhav 10-11-2022 13:26");
+
             Console.ReadLine();
         }
     }
