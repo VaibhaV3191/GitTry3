77de8c82fdaba745c02bc5336d22a85bd1e554cb
VaibhaV3191 <vaibhav.shinde@vhaashtech.com>
2022-11-10 13:02:23 +0530

    Add project files.

---
+++ GitTry3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTry3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Master");
+            Console.ReadLine();
+        }
+    }
+}
