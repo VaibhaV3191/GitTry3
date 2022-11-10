a81e25970eb89562a24c10e91e410646d88f9902
VaibhaV3191 <vaibhav.shinde@vhaashtech.com>
2022-11-10 13:02:23 +0530

    DoneAdd project files.

---
+++ GitTry3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitTry3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Master");
+            Console.WriteLine("Hello Sharad");
+            Console.ReadLine();
+        }
+    }
+}
