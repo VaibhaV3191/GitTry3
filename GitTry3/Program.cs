130ebaeb4804fd34724593b99d09d5b2d6f1b5d9
NikhilDevkate <nikhildevkate11@gmail.com>
2022-11-10 17:39:52 +0530

    Changes by Nikhil

--- GitTry3/Program.cs
+++ GitTry3/Program.cs
@@ -7,6 +7,7 @@ namespace GitTry3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Master");
+            Console.WriteLine("Hello Master Nikhil");
             Console.ReadLine();
         }
     }
